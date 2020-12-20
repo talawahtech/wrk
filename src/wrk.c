8d14d385d080271d0ed19eeacb731454cae9a8e7
Marc Richards <talawahtech@users.noreply.github.com>
2020-12-19 23:26:42 -0500

    Add p9999 to latency distribution stats and remove p75

--- src/wrk.c
+++ src/wrk.c
@@ -640,12 +640,12 @@ static void print_stats(char *name, stats *stats, char *(*fmt)(long double)) {
 }
 
 static void print_stats_latency(stats *stats) {
-    long double percentiles[] = { 50.0, 75.0, 90.0, 99.0 };
+    long double percentiles[] = { 50.0, 90.0, 99.0, 99.99 };
     printf("  Latency Distribution\n");
     for (size_t i = 0; i < sizeof(percentiles) / sizeof(long double); i++) {
         long double p = percentiles[i];
         uint64_t n = stats_percentile(stats, p);
-        printf("%7.0Lf%%", p);
+        printf("%7.2Lf%%", p);
         print_units(n, format_time_us, 10);
         printf("\n");
     }
